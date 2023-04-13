41ee1db0c43bee5a6f2d7806e077af9fae364f48
BrenoOPrado <breno.o.prado@hotmail.com>
2023-04-13 14:37:37 -0300

    Iniciando desenvolvimento do projeto :turtle:

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,10 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+ingredient = Ingredient('Testando')
 
 
 # Req 1
 def test_ingredient():
+    print('/-----------------------------------------/')
+    print(ingredient.__eq__())
+    print('/-----------------------------------------/')
     pass
